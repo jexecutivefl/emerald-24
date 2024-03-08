55a6b96f6522b797703ec36550b0589ce6ec5ab2
Jacob Greenberg <jacob.greenberg45@gmail.com>
2024-03-08 11:35:21 -0500

    commit

--- views/Home/HomeDesktop/components/CardComponent2View/CardComponent2View.jsx
+++ views/Home/HomeDesktop/components/CardComponent2View/CardComponent2View.jsx
@@ -7,24 +7,27 @@ import icon4 from '/public/insomnia.png';
 import icon5 from '/public/muscle-spasms.png';
 import icon6 from '/public/nausea.png';
 import icon7 from '/public/glaucoma.png';
+import Section from "@/components/Section/Section";
 
 
 
 
 const CardComponent2View = () => {
     return (
-        <div className={styles.container}>
-            <h2 className={styles.h2}>Medical Benefits</h2>
-            <div className={styles.cardContainer}>
-                <CardComponent2 icon={icon2} title="Anxiety Reduction" />
-                <CardComponent2 icon={icon3} title="Sleep Improvement" />
-                <CardComponent2 icon={icon4} title="Stress Relief" />
-                <CardComponent2 icon={icon5} title="Pain Relief" />
-                <CardComponent2 icon={icon6} title="Nause Control" />
-                <CardComponent2 icon={icon7} title="Inflammation Reduction" />
-                {/* Add more CardComponent2 instances as needed */}
+        <Section>
+            <div className={styles.container}>
+                <h2 className={styles.h2}>Medical Benefits</h2>
+                <div className={styles.cardContainer}>
+                    <CardComponent2 icon={icon2} title="Anxiety Reduction" />
+                    <CardComponent2 icon={icon3} title="Sleep Improvement" />
+                    <CardComponent2 icon={icon4} title="Stress Relief" />
+                    <CardComponent2 icon={icon5} title="Pain Relief" />
+                    <CardComponent2 icon={icon6} title="Nause Control" />
+                    <CardComponent2 icon={icon7} title="Inflammation Reduction" />
+                    {/* Add more CardComponent2 instances as needed */}
+                </div>
             </div>
-        </div>
+        </Section>
     );
 };
 
